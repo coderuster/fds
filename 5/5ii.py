bcb138c4ca17e3e28dafd7fe0bd0f00b63aac0c9
coderuster <165394336+coderuster@users.noreply.github.com>
2024-10-02 21:34:04 +0530

    Update 5ii.py

--- 5/5ii.py
+++ 5/5ii.py
@@ -1,16 +1,14 @@
-import pandas as pd  # type: ignore
-import matplotlib.pyplot as plt  # type: ignore
-
+import plotly.express as px 
 cities = ['London', 'New York', 'Tokyo', 'Sydney', 'Cairo']
-lats = [51.5074, 40.7128, 35.6762, -33.8688, 30.0444]
-longs = [-0.1278, -74.0060, 139.6503, 151.2093, 31.2357]
+latitudes = [51.5074, 40.7128, 35.6762, -33.8688, 30.0444]
+longitudes = [-0.1278, -74.0060, 139.6503, 151.2093, 31.2357]
 
-plt.scatter(longs, lats)
-plt.title("Major Cities")
-plt.xlabel("Longitude")
-plt.ylabel("Latitude")
+city_data = pd.DataFrame({
+    'City': cities,
+    'Latitude': latitudes,
+    'Longitude': longitudes
+})
 
-for i, city in enumerate(cities):
-    plt.text(longs[i], lats[i], city)
+fig = px.scatter_geo(city_data, lat='Latitude', lon='Longitude', hover_name='City',projection="natural earth", title="Major Cities for Tourism Promotion")
 
-plt.savefig("cities_visualization.png")
+fig.show()
